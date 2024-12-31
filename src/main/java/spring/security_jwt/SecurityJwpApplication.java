a544fa233c8ea6fb997280f29ec70ee45297c34b
DouglasSimoesM <douglas.simoesm@gmail.com>
2024-12-31 08:38:35 -0300

    JWT Criptografia

---
+++ src/main/java/spring/security_jwt/SecurityJwpApplication.java
@@ -0,0 +1,13 @@
+package spring.security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwpApplication.class, args);
+	}
+
+}
